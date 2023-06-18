661c54c0e741176fc1ad47734a47bd5e7d085645
Michael Head <michaelehead@gmail.com>
2023-06-18 07:13:37 -0400

    remove .env file, use netlify conditional

--- src/routes/api/s3/+server.ts
+++ src/routes/api/s3/+server.ts
@@ -1,11 +1,17 @@
 import { json } from '@sveltejs/kit';
 import type { RequestEvent } from "./$types";
 import { S3Client, ListObjectsV2Command, type ListObjectsV2CommandInput } from '@aws-sdk/client-s3';
-import { env } from '$env/dynamic/private';
 
-let client = new S3Client({ credentials: { accessKeyId: env.AWS_ACCESS_KEY_ID, secretAccessKey: env.AWS_SECRET_ACCESS_KEY }, region: 'us-east-2' });
+let client: S3Client;
 
 const defaultRegion = "us-east-2";
+
+if (process.env.$NETLIFY === "true") {
+	// `AWS_ACCESS_KEY_ID` is a reserved ENV variable in Netlify, so use a prefixed version there.
+	client = new S3Client({ credentials: { accessKeyId: process.env.SVELTE_AWS_ACCESS_KEY_ID as string, secretAccessKey: process.env.SVELTE_AWS_SECRET_ACCESS_KEY as string }, region: 'us-east-2' });
+} else {
+	client = new S3Client({ region: defaultRegion })
+}
 
 export async function GET({ url }: RequestEvent) {
 	const path = url.searchParams.get('path') as string;
